9e19f89fe8f492d3346aea17a424a14a93fb7fb1
Parapets <southwest85b@gmail.com>
2021-07-28 13:13:36 +0100

    Update pyro build to handle VR and separate SDK

---
+++ PreBuild.bat
@@ -0,0 +1,14 @@
+xcopy "dist\Data" "dist\SkyrimSE" /exclude:xcopy_exclude.txt /s /y
+xcopy "dist\Data" "dist\SkyrimVR" /exclude:xcopy_exclude.txt /s /y
+
+cmake -B build -S . -DBUILD_SKYRIMVR=OFF
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build build --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
+xcopy "build\Release\%1.dll" "dist\SkyrimSE\SKSE\Plugins\%1.dll*" /y
+
+cmake -B buildVR -S . -DBUILD_SKYRIMVR=ON
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build buildVR --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
+xcopy "buildVR\Release\%1.dll" "dist\SkyrimVR\SKSE\Plugins\%1.dll*" /y
