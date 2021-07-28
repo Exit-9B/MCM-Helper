811de3b8da1e86e60bc96a4803bfbf0a664d73ae
Parapets <southwest85b@gmail.com>
2021-07-28 14:18:00 +0100

    More build process improvements

--- PreBuild.bat
+++ PreBuild.bat
@@ -1,14 +1,18 @@
-xcopy "dist\Data" "dist\SkyrimSE" /exclude:xcopy_exclude.txt /s /y
-xcopy "dist\Data" "dist\SkyrimVR" /exclude:xcopy_exclude.txt /s /y
+@echo off
 
 cmake -B build -S . -DBUILD_SKYRIMVR=OFF
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build build --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
-xcopy "build\Release\%1.dll" "dist\SkyrimSE\SKSE\Plugins\%1.dll*" /y
 
 cmake -B buildVR -S . -DBUILD_SKYRIMVR=ON
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build buildVR --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
+
+xcopy "dist\Data" "dist\SkyrimSE" /exclude:xcopy_exclude.txt /s /y
+xcopy "dist\Data" "dist\SkyrimVR" /exclude:xcopy_exclude.txt /s /y
+
+xcopy "build\Release\%1.dll" "dist\SkyrimSE\SKSE\Plugins\%1.dll*" /y
+
 xcopy "buildVR\Release\%1.dll" "dist\SkyrimVR\SKSE\Plugins\%1.dll*" /y
