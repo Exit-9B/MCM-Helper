7acb6a058530f2e4fabb804919e3f948830cebbb
Parapets <southwest85b@gmail.com>
2021-07-30 19:05:46 +0100

    Use temporary folder for builds

--- PreBuild.bat
+++ PreBuild.bat
@@ -1,14 +1,19 @@
 @echo off
 
 cmake -B build -S . -DBUILD_SKYRIMVR=OFF --check-stamp-file "build/CMakeFiles/generate.stamp"
+if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build build --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
 
 cmake -B buildVR -S . -DBUILD_SKYRIMVR=ON --check-stamp-file "buildVR/CMakeFiles/generate.stamp"
+if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build buildVR --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
 
-xcopy "dist\Data" "dist\SkyrimSE" /exclude:xcopy_exclude.txt /s /y
-xcopy "dist\Data" "dist\SkyrimVR" /exclude:xcopy_exclude.txt /s /y
+xcopy "dist\Data" "%2\SkyrimSE" /exclude:xcopy_exclude.txt /i /s /y
+xcopy "dist\Data" "%2\SkyrimVR" /exclude:xcopy_exclude.txt /i /s /y
+xcopy "dist\SkyrimSE" "%2\SkyrimSE" /i /s /y
+xcopy "dist\SkyrimVR" "%2\SkyrimVR" /i /s /y
 
-xcopy "build\Release\%1.dll" "dist\SkyrimSE\SKSE\Plugins\%1.dll*" /y
-
-xcopy "buildVR\Release\%1.dll" "dist\SkyrimVR\SKSE\Plugins\%1.dll*" /y
+xcopy "build\Release\%1.dll" "%2\SkyrimSE\SKSE\Plugins\%1.dll*" /y
+xcopy "buildVR\Release\%1.dll" "%2\SkyrimVR\SKSE\Plugins\%1.dll*" /y
