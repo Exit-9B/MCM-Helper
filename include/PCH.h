5847eb115c26f092bea3712233ca0038b89cd353
Parapets <southwest85b@gmail.com>
2021-07-30 21:00:49 +0100

    Set plugin version code to 1

--- include/PCH.h
+++ include/PCH.h
@@ -5,7 +5,7 @@
 #include <unordered_set>
 #include <tsl/ordered_map.h>
 
-constexpr std::int32_t PLUGIN_VERSION = 0; // Pre-release
+constexpr std::int32_t PLUGIN_VERSION = 1;
 
 #ifdef NDEBUG
 #include <spdlog/sinks/basic_file_sink.h>
