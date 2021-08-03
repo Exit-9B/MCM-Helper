63f89ed570a5a77c7effff7d9c63d29e86638da9
Parapets <southwest85b@gmail.com>
2021-08-03 13:55:54 +0100

    Simplifying build process

--- PreBuild.bat
+++ PreBuild.bat
@@ -10,10 +10,10 @@ if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build buildVR --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
 
-xcopy "dist\Data" "%2\SkyrimSE" /exclude:xcopy_exclude.txt /i /s /y
-xcopy "dist\Data" "%2\SkyrimVR" /exclude:xcopy_exclude.txt /i /s /y
-xcopy "dist\SkyrimSE" "%2\SkyrimSE" /i /s /y
-xcopy "dist\SkyrimVR" "%2\SkyrimVR" /i /s /y
+xcopy "dist\Data" "%2\SkyrimSE" /i /s /y
+xcopy "dist\Data" "%2\SkyrimVR" /i /s /y
+xcopy "dist\SkyrimSE" "%2\SkyrimSE" /s /y
+xcopy "dist\SkyrimVR" "%2\SkyrimVR" /s /y
 
 xcopy "build\Release\%1.dll" "%2\SkyrimSE\SKSE\Plugins\%1.dll*" /y
 xcopy "buildVR\Release\%1.dll" "%2\SkyrimVR\SKSE\Plugins\%1.dll*" /y
