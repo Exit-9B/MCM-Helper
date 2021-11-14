31c9a1d50732dc58052da68fb9c750ebb1896ba3
Parapets <southwest85b@gmail.com>
2021-11-14 20:58:01 +0000

    Fix logger not initializing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,43 @@
 #include "KeybindEventHandler.h"
 #include "KeybindManager.h"
 
+void InitLogger()
+{
+	static bool initialized = false;
+	if (!initialized) {
+		initialized = true;
+	}
+	else {
+		return;
+	}
+
+#ifndef NDEBUG
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+	auto path = logger::log_directory();
+	if (!path) {
+		return;
+	}
+
+	*path /= fmt::format("{}.log"sv, Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+#ifndef NDEBUG
+	log->set_level(spdlog::level::trace);
+#else
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::warn);
+#endif
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%s(%#): [%^%l%$] %v"s);
+
+	logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
+}
+
 #ifndef SKYRIMVR
 
 extern "C" DLLEXPORT SKSE::PluginVersionData SKSEPlugin_Version =
@@ -27,31 +64,7 @@ extern "C" DLLEXPORT SKSE::PluginVersionData SKSEPlugin_Version =
 extern "C" DLLEXPORT bool SKSEAPI
 	SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-#ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-	auto path = logger::log_directory();
-	if (!path) {
-		return false;
-	}
-
-	*path /= fmt::format("{}.log"sv, Version::PROJECT);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-#ifndef NDEBUG
-	log->set_level(spdlog::level::trace);
-#else
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::warn);
-#endif
-
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("%s(%#): [%^%l%$] %v"s);
-
-	logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
+	InitLogger();
 
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
 	a_info->name = Version::PROJECT.data();
@@ -76,6 +89,8 @@ extern "C" DLLEXPORT bool SKSEAPI
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
+	InitLogger();
+
 	logger::info("{} loaded"sv, Version::PROJECT);
 
 	SKSE::Init(a_skse);
