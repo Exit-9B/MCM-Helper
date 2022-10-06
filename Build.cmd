f9b8c1653cfe03d0a15b9a2eebe46d0f64a98ff9
Parapets <southwest85b@gmail.com>
2022-10-06 16:48:16 +0100

    Drop VS2019 presets

--- Build.cmd
+++ Build.cmd
@@ -1,15 +1,11 @@
 @echo off
 
-md "package\SkyrimSE"
-md "package\SkyrimVR"
-md "package\SDK"
-
-cmake --preset vs2019-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
+cmake --preset vs2022-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
 cmake --build build --config Release || goto :error
 cmake --install build --prefix "package/SkyrimSE" || goto :error
 cmake --install build --component "SDK" --prefix "package/SDK" || goto :error
 
-cmake --preset vs2019-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
+cmake --preset vs2022-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
 cmake --build buildVR --config Release || goto :error
 cmake --install buildVR --prefix "package/SkyrimVR" || goto :error
 
