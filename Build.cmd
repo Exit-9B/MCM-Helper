48b2b7cfad32067bc34b1ae5c72936ddd3ae716a
Parapets <southwest85b@gmail.com>
2023-12-17 17:36:22 +0000

    Create FOMOD installer

--- Build.cmd
+++ Build.cmd
@@ -1,20 +1,27 @@
 @echo off
 
-cmake --preset vs2022-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
+md "package\Fomod"
+md "package\SDK"
+
+cmake --preset vs2022-windows --check-stamp-file "build/CMakeFiles/generate.stamp" -Wno-deprecated || goto :error
 cmake --build build --config Release || goto :error
-cmake --install build --prefix "package/SkyrimSE" || goto :error
+cmake --install build --component "SKSEPlugin" --prefix "package/Fomod/SkyrimSE" || goto :error
+
+cmake --preset vs2022-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" -Wno-deprecated || goto :error
+cmake --build buildVR --target "MCMHelper" --config Release || goto :error
+cmake --install buildVR --component "SKSEPlugin" --prefix "package/Fomod/SkyrimVR" || goto :error
+
+cmake --install build --component "Fomod" --prefix "package/Fomod" || goto :error
+cmake --install build --component "ESP" --prefix "package/Fomod/ESP" || goto :error
+cmake --install build --component "ESL" --prefix "package/Fomod/ESL" || goto :error
+cmake --install build --component "BSA" --prefix "package/Fomod/BSA" || goto :error
+cmake --install build --component "Loose" --prefix "package/Fomod/Loose" || goto :error
+cmake --install build --component "Data" --prefix "package/Fomod/Data" || goto :error
 cmake --install build --component "SDK" --prefix "package/SDK" || goto :error
 
-cmake --preset vs2022-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
-cmake --build buildVR --config Release || goto :error
-cmake --install buildVR --prefix "package/SkyrimVR" || goto :error
-
 pushd package
-pushd SkyrimSE
-7z a -r -t7Z "..\MCM.Helper.SE.7z"
-popd
-pushd SkyrimVR
-7z a -r -t7Z "..\MCM.Helper.VR.7z"
+pushd Fomod
+7z a -r -t7Z "..\MCM.Helper.7z"
 popd
 pushd SDK
 7z a -r -t7Z "..\MCM.SDK.7z"
--- install_files.cmake
+++ install_files.cmake
@@ -1,53 +1,72 @@
-if(NOT BUILD_SKYRIMVR)
-	install(
-		FILES
-			"${CMAKE_CURRENT_SOURCE_DIR}/dist/SkyrimSE/MCMHelper.esp"
-		DESTINATION "."
-	)
-else()
-	install(
-		FILES
-			"${CMAKE_CURRENT_SOURCE_DIR}/dist/SkyrimVR/MCMHelper.esp"
-		DESTINATION "."
-	)
-endif()
+install(
+	FILES
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/ESP/MCMHelper.esp"
+	DESTINATION "."
+	COMPONENT ESP
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/ESL/MCMHelper.esp"
+	DESTINATION "."
+	COMPONENT ESL
+)
 
 install(
 	FILES
 		"${CMAKE_CURRENT_BINARY_DIR}/MCMHelper.bsa"
 	DESTINATION "."
+	COMPONENT BSA
+)
+
+install(
+	FILES ${TRANSLATION_FILES}
+	DESTINATION "Interface/Translations"
+	COMPONENT Loose
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES ${Papyrus_OUTPUT}
+	DESTINATION "Scripts"
+	COMPONENT Loose
+	EXCLUDE_FROM_ALL
 )
 
 install(
 	FILES
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/MCM/Settings/readme.txt"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/MCM/Settings/readme.txt"
 	DESTINATION
 		"MCM/Settings"
+	COMPONENT Data
 )
 
 install(
 	FILES
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/MCM/Config/SkyUI_SE/config.json"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/MCM/Config/SkyUI_SE/settings.ini"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/MCM/Config/SkyUI_SE/config.json"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/MCM/Config/SkyUI_SE/settings.ini"
 	DESTINATION
 		"MCM/Config/SkyUI_SE"
+	COMPONENT Data
 )
 
 install(
 	FILES
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Source/Scripts/SKI_ConfigMenu.psc"
+		"${CMAKE_CURRENT_SOURCE_DIR}/scripts/public/SKI_ConfigMenu.psc"
 	DESTINATION
 		"Source/Scripts"
+	COMPONENT Data
 )
 
 install(
 	FILES
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/MCM.pex"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/MCM_ConfigBase.pex"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/SKI_ConfigBase.pex"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/SKI_ConfigManager.pex"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/SKI_PlayerLoadGameAlias.pex"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Scripts/SKI_QuestBase.pex"
+		"${CMAKE_CURRENT_BINARY_DIR}/Scripts/MCM.pex"
+		"${CMAKE_CURRENT_BINARY_DIR}/Scripts/MCM_ConfigBase.pex"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/SDK/Scripts/SKI_ConfigBase.pex"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/SDK/Scripts/SKI_ConfigManager.pex"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/SDK/Scripts/SKI_PlayerLoadGameAlias.pex"
+		"${CMAKE_CURRENT_SOURCE_DIR}/data/SDK/Scripts/SKI_QuestBase.pex"
 	DESTINATION "Scripts"
 	COMPONENT SDK
 	EXCLUDE_FROM_ALL
@@ -55,11 +74,30 @@ install(
 
 install(
 	FILES
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Source/Scripts/MCM.psc"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Source/Scripts/MCM_ConfigBase.psc"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Source/Scripts/SKI_ConfigBase.psc"
-		"${CMAKE_CURRENT_SOURCE_DIR}/dist/Data/Source/Scripts/SKI_QuestBase.psc"
+		"${CMAKE_CURRENT_SOURCE_DIR}/scripts/public/MCM.psc"
+		"${CMAKE_CURRENT_SOURCE_DIR}/scripts/public/MCM_ConfigBase.psc"
+		"${CMAKE_CURRENT_SOURCE_DIR}/scripts/public/SKI_ConfigBase.psc"
+		"${CMAKE_CURRENT_SOURCE_DIR}/scripts/public/SKI_QuestBase.psc"
 	DESTINATION "Source/Scripts"
 	COMPONENT SDK
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${PROJECT_SOURCE_DIR}/data/fomod/info.xml"
+		"${PROJECT_SOURCE_DIR}/data/fomod/ModuleConfig.xml"
+	DESTINATION "fomod"
+	COMPONENT Fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${PROJECT_SOURCE_DIR}/data/fomod/images/mcmhelper.jpg"
+		"${PROJECT_SOURCE_DIR}/data/fomod/images/skyrim_se.jpg"
+		"${PROJECT_SOURCE_DIR}/data/fomod/images/skyrim_vr.jpg"
+	DESTINATION "fomod/images"
+	COMPONENT Fomod
 	EXCLUDE_FROM_ALL
 )
